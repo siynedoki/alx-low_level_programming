c381d1ae69c304eb25488e666251d43c63d11500
siynedoki <edokisamuel@yahoo.com>
2023-06-15 19:28:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main - A c program that prints the size of the c types
-* Return: 0 (success)
+* main - A c program that prints the size of various computer types
+* Return: 0 (Success)
 */
 int main(void)
 {
