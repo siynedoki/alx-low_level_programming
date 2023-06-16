ab32e27ccf431886026124550533f8a1180a2a70
siynedoki <edokisamuel@yahoo.com>
2023-06-16 16:46:03 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of code
+ * Return: 0 Always (Success)
+*/
+
+int main(void)
+{
+	char p;
+	char o;
+
+	p = 'a';
+	o = 'A';
+	while
+	(p <= 'z')
+	{
+		putchar(p);
+		p++;
+	}
+	while
+	(o <= 'Z')
+	{
+		putchar(o);
+		o++;
+	}
+	putchar('\n');
+	return (0);
+}
