dcbd82110fa7a9dd6c1713e92ada741b4e86de8e
siynedoki <edokisamuel@yahoo.com>
2023-06-17 04:04:07 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,25 +25,18 @@ int main(void)
 		{
 
 				if (l > m && m > n)
-
 				{
-
 					putchar(n);
-
 					putchar(m);
 					putchar(l);
-
 					if (n != 55 || m != 56)
-
 					{
 
 						putchar(',');
 						putchar(' ');
 					}
 				}
-
 			}
-
 		}
 
 	}
