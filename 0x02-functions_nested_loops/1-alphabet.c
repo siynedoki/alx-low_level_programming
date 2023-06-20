85f9c3e94bd3ad0c85ca5321323371c3a1bce602
siynedoki <edokisamuel@yahoo.com>
2023-06-20 14:05:34 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point of code
+ * Return: 0 (successful)
+ */
+
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point of code
+ * Return: 0 (successful)
+ */
+
+void print_alphabet_x10(void)
+{
+        char n;
+
+        for (n = 'a'; n <= 'z'; n++)
+        {
+		{
+		       int i; 
+		       	       for (i = '0'; i > '10'; i++)
+				_putchar(n);
+		}
+		}
+        _putchar('\n');
+}
