a78fe1e984dc30e86fd5312bac39d9ca63b3548f
siynedoki <edokisamuel@yahoo.com>
2023-06-21 07:31:45 -0700

    done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - validates if a character is a letter,
+ * upper or lower case
+ *
+ * @c: to be expressed in ASCI value of charaster
+ * Return: 1 (succesful) else, 0
+ */
+
+int _isalpha(int c)
+{
+	if (c <= 65 && c >= 122)
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
