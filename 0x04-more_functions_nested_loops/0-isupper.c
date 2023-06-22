c4323bd11dad97ba2a8b022e585514782ba21c6b
siynedoki <edokisamuel@yahoo.com>
2023-06-22 14:53:37 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case
+ *@c: inputed character
+ *Return: 1 (uppercase) else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 41 && c <= 90)
+	return (1);
+
+	else
+	return (0);
+}
